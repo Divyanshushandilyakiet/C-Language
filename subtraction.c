7a4309f70890bef53dc628d261db85dee6afc9b9
Divyanshu shandilya <70764631+Divyanshushandilyakiet@users.noreply.github.com>
2020-10-28 19:54:57 +0530

    Add files via upload

---
+++ subtraction.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main()
+{
+	int a,b,sub;
+
+
+	printf("Enter the first no.: ");
+	scanf("%d",&a);
+
+
+	printf("Enter the second no.: ");
+	scanf("%d",&b);
+
+
+	sub= a-b;
+	printf("subtract is = %d\n", sub);
+
+	return 0;
+}
