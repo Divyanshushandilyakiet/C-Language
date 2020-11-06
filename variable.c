6ad4d451bc6560c0179a4001180625fd262bb11c
Divyanshu shandilya <70764631+Divyanshushandilyakiet@users.noreply.github.com>
2020-11-06 19:39:38 +0530

    Add files via upload

---
+++ variable.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main ()
+{
+    int x,y,z;
+    x = 20;
+    y = 30;
+    z = x + y;
+
+
+    printf ("value of x = %d, y = %d and z = %d\n", x, y, z);
+
+    return 0;
+}
