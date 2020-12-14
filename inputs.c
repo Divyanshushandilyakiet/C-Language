e3c949f4dc6ac7fcbd677af44d85c70c5af2afe3
Divyanshu shandilya <70764631+Divyanshushandilyakiet@users.noreply.github.com>
2020-12-14 20:40:37 +0530

    Add files via upload

---
+++ inputs.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+int main(int argc, char const *argv[])
+{
+    char data;
+    int num;
+    printf("Enter any number ");
+    scanf("%d",&num);
+    printf("your number is=%d",num);
+  
+    return 0;
+}
