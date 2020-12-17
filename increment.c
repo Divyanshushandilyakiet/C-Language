54c28c9c68bd592bc5dd25e875e9817b1fdeb3dd
Divyanshu shandilya <70764631+Divyanshushandilyakiet@users.noreply.github.com>
2020-12-17 21:09:15 +0530

    Add files via upload

---
+++ increment.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    int a = 10;
+    int b = 100;
+    float c = 10.5;
+    float d = 100.5;
+
+    printf("++a = %d \n", ++a);
+    printf("--b = %d \n", --b);
+    printf("++c = %f \n", ++c);
+    printf("--d = %f \n", --d);
+
+    return 0;
+}
