fe92df5e8f2a57f273284d3992c05f453703a260
Divyanshu shandilya <70764631+Divyanshushandilyakiet@users.noreply.github.com>
2020-12-20 22:58:02 +0530

    Update if.c

--- if.c
+++ if.c
@@ -6,13 +6,21 @@ int main(int argc, char const *argv[])
     scanf("%d",&num1);
     printf("Enter the number2=");
     scanf("%d",&num2);
-    if(num1>num2)
+    printf("Enter the number3=");
+    scanf("%d",&num3);
+
+    if(num1>num2 && num1>num3)
     {
         printf("Number1=%d is greater",num1);
     }
+    else if (num2>num1 && num2>num3)
+    {
+        printf("Number2=%d is greater",num2);
+    }
     else
     {
-        printf("number2=%d is greater",num2);
+        printf("Number3=%d is greater",num3);
     }
+    
     return 0;
 }
