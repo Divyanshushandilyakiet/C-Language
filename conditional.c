f96801f59f0b58f9af93baea2a73d4fcd99ae604
Divyanshu shandilya <70764631+Divyanshushandilyakiet@users.noreply.github.com>
2020-12-23 22:45:43 +0530

    Add files via upload

---
+++ conditional.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(int argc, char const *argv[])
+{
+    int x=10;
+    printf("welcome the confitionl statement.....");
+    if(x==10)
+    {
+        printf("\nThis is for x equal to 10");
+    }
+    return 0;
+}
