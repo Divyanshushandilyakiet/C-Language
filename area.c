20c0111d58b8dcf92b1a6e7a61aa5876efc12441
Divyanshu shandilya <70764631+Divyanshushandilyakiet@users.noreply.github.com>
2020-12-29 05:54:33 +0530

    Update area.c

--- area.c
+++ area.c
@@ -1,5 +1,13 @@
-#include <stdio.h>
-int main(){
-printf("Hello World!");
-}
+#include<stdio.h>
+int main()
+{
+    int side,area;
 
+    printf("\n Enter the Length of Side : ");
+    scanf("%d",&side);
+
+    area = side * side ;
+
+    printf("\n Area of Square : %d",area);
+    return 0;
+}
