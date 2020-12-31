d5af2daac4266728041be2169fd55123e1bd853f
Divyanshu shandilya <70764631+Divyanshushandilyakiet@users.noreply.github.com>
2020-12-31 22:36:58 +0530

    Update circle.c

--- circle.c
+++ circle.c
@@ -1,1 +1,10 @@
-
+#include <stdio.h>
+int main() 
+{
+   float pie = 3.14;
+   int radius = 6;
+   printf("The radius of the circle is %d \n" , radius);
+   float area = (float)(pie* radius * radius);
+   printf("The area of the given circle is %f", area);
+   return 0;
+}
