c9979f579930f1c3dad520c5dfcb966a7da29fc3
Divyanshu shandilya <70764631+Divyanshushandilyakiet@users.noreply.github.com>
2021-01-04 21:18:36 +0530

    Add files via upload

---
+++ square.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(int argc, char const *argv[])
+{
+    int x;
+    printf("Enter a number =  ");
+    scanf("%d",&x);
+    printf("Square of %d is %d",x,x*x);
+    return 0;
+}
