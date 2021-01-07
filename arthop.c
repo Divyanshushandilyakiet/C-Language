9e16c9e621d06c06d1e41b296973a4987f1dcf6d
Divyanshu shandilya <70764631+Divyanshushandilyakiet@users.noreply.github.com>
2021-01-07 22:10:47 +0530

    Add files via upload

---
+++ arthop.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(int argc, char const *argv[])
+{
+    int x,y,z,a,b;
+    x=25+2;y=25-2;z=25*2;a=25%2;b=25/2;
+    printf("%d\n%d\n%d\n%d\n%d\n",x,y,z,a,b);
+    return 0;
+}
