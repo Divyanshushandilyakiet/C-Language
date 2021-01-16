e0948d19bb173371133ad84c9905ccb9dd0e8ec4
Divyanshu shandilya <70764631+Divyanshushandilyakiet@users.noreply.github.com>
2021-01-16 22:23:50 +0530

    Add files via upload

---
+++ asciival.c
@@ -0,0 +1,11 @@
+
+#include<stdio.h>
+int main ()
+{
+    char ch;
+printf ("Enter any character to know their ASCII value = >");
+scanf ("%c", &ch);
+
+printf ("ASCII value of ' %c ' is => %d", ch, ch);
+return 0;
+}
