1294f6a570c982a058921c50e0d67d7ccefe478c
Divyanshu shandilya <70764631+Divyanshushandilyakiet@users.noreply.github.com>
2021-01-20 23:25:15 +0530

    Add files via upload

---
+++ addtw.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(int argc, char const *argv[])
+{
+int number;
+float amount;
+
+number= 100;
+
+
+amount =30.75+75.35;
+printf("%d"\n,number);
+printf("%5.2f",amount);
+    return 0;
+}
