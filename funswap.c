944a4fc7eebf38e76780cdfc65ea43dbdc573f83
Divyanshu shandilya <70764631+Divyanshushandilyakiet@users.noreply.github.com>
2021-01-31 22:55:01 +0530

    Add files via upload

---
+++ funswap.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void swap(int *, int *);
+int main(int argc, char const *argv[])
+{
+    int n1,n2;
+    printf("\n\n Function : swap two numbers using function : \n");
+    printf("Input first number : ");
+    scanf("%d",&n1);
+    printf("Intput secon number : ");
+    scanf("%d",&n2);
+
+    printf ("Before ")
+    return 0;
+}
+
