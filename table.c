84021243415531b53063c056b240d99c5d9f6a94
Divyanshu shandilya <70764631+Divyanshushandilyakiet@users.noreply.github.com>
2021-02-14 23:48:50 +0530

    Add files via upload

---
+++ table.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(int argc, char const *argv[])
+{
+    int i,j;
+    for(i=1;i<=10;i++)
+    {
+        for(j=1;j<=10;j++)
+        {
+        printf("\t%d",i*j);
+        }
+        printf("\n");
+    }
+    return 0;
+}
