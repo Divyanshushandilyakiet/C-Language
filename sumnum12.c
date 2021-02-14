fb1546b81f256993a2d22fe4e5f5c0714a963e01
Divyanshu shandilya <70764631+Divyanshushandilyakiet@users.noreply.github.com>
2021-02-14 23:50:03 +0530

    Add files via upload

---
+++ sumnum12.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main ()
+{
+    int number1, number2, sum;
+
+printf ("Enter the value for number 1 =>");
+scanf ("%d", &number1);
+
+printf ("Enter the value for number 2 =>");
+scanf ("%d", &number2);
+
+    sum= number1+number2;
+
+printf ("Addition of %d and %d is => %d", number1, number2, sum);
+    return 0;
+}
+
