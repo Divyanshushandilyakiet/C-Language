a0758dfb7a5d656acec288a5dd8b92d49876e4f0
Divyanshu shandilya <70764631+Divyanshushandilyakiet@users.noreply.github.com>
2021-02-17 22:55:39 +0530

    Add files via upload

---
+++ simpleints.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(int argc, char const *argv[])
+{
+    int p, n;
+    float r, si;
+
+    p=23;
+    n=3;
+    r=4;
+
+    si=p*r*n/100;
+    printf("%f",si);
+    return 0;
+}
