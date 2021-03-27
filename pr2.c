4d04aa897983cfb3ecbbabecb576e6829bb6e0f8
Divyanshu shandilya <70764631+Divyanshushandilyakiet@users.noreply.github.com>
2021-03-27 23:04:55 +0530

    Add files via upload

---
+++ pr2.c
@@ -0,0 +1,12 @@
+/* program to print 1 to 100 positive integers*/
+#include<stdio.h>
+int main(int argc, char const *argv[])
+{
+    int i;
+    printf("print 1 to 100 positive integers\n");
+    for(i=1;i<=100;i++)
+    {
+        printf("%d\n",i);
+    }
+    return 0;
+}
