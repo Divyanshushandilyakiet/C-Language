c75a0dd2d96c870794d72c1dfee6cf7c7018a605
Divyanshu shandilya <70764631+Divyanshushandilyakiet@users.noreply.github.com>
2021-03-31 21:47:02 +0530

    Add files via upload

---
+++ pr3.c
@@ -0,0 +1,12 @@
+/* program to print A to Z alphbets */
+#include<stdio.h>
+int main(int argc, char const *argv[])
+{
+    int i;
+    printf("alphabets A to Z\n");
+    for(i=65;i<=90;i++)
+    {
+        printf("%c\n",i);
+    }
+    return 0;
+}
