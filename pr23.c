6741064e98f2bf9c176a84d59330bc42296b0d02
Divyanshu shandilya <70764631+Divyanshushandilyakiet@users.noreply.github.com>
2021-04-10 22:34:53 +0530

    Add files via upload

---
+++ pr23.c
@@ -0,0 +1,14 @@
+// program to print the lowercase alphabets
+#include <stdio.h>
+int main(int argc, char const *argv[])
+{
+
+    char ch;
+    printf("ALPHABETS a TO z ARE AS FOLLOW:\n");
+    for (ch = 'a'; ch <= 'z'; ch++)
+    {
+        printf("%c\n", ch);
+    }
+
+    return 0;
+}
