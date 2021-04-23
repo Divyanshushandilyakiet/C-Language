0e344f29ff017698c17186c733be96127fde531b
Divyanshu shandilya <70764631+Divyanshushandilyakiet@users.noreply.github.com>
2021-04-23 20:05:16 +0530

    Add files via upload

---
+++ pr32.c
@@ -0,0 +1,14 @@
+//A simple structure of function
+#include <stdio.h>
+int main(int argc, char const *argv[])
+{
+    /* code */
+    return 0;
+}
+int sum(int a, int b)
+{
+    int s;
+    s=a+b;
+    return s;
+    
+}
