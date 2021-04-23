075cde108f1743fa33a560a709efd6e9ca79ba29
Divyanshu shandilya <70764631+Divyanshushandilyakiet@users.noreply.github.com>
2021-04-23 20:04:49 +0530

    Add files via upload

---
+++ pr30.c
@@ -0,0 +1,13 @@
+// Program to print the natural numbers
+#include <stdio.h>
+int main(int argc, char const *argv[])
+{
+
+    int n = 1;
+    while (n <= 10)
+    {
+        printf("%d\n", n);
+        n++;
+    }
+    return 0;
+}
