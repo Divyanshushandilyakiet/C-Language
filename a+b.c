3ce76e89124302340dff7f6a3048b9eaaf8f4388
Divyanshu shandilya <70764631+Divyanshushandilyakiet@users.noreply.github.com>
2021-06-05 16:37:45 +0530

    Add files via upload

---
+++ a+b.c
@@ -0,0 +1,12 @@
+// Sum of two numbers
+#include<stdio.h>
+int main(int argc, char const *argv[])
+{
+    int a,b,c;
+    printf("Enter two numbers==>");
+    scanf("%d%d",&a,&b);
+    c=a+b;
+    printf("The sum of two numbers are %d",c);
+
+    return 0;
+}
