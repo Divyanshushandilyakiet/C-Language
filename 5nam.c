aeaf2a27f34c99ceb28d10489db830fb00827f0e
Divyanshu shandilya <70764631+Divyanshushandilyakiet@users.noreply.github.com>
2021-06-06 14:36:55 +0530

    Add files via upload

---
+++ 5nam.c
@@ -0,0 +1,15 @@
+// print name n times 
+#include<stdio.h>
+int main(int argc, char const *argv[])
+{
+    int n,i=1;
+    printf("Enter number of times--->>");
+    scanf("%d",&n);
+
+    while(i<=n)
+    {
+        printf("Divyanshu\n");
+        i++;
+    }
+    return 0;
+}
